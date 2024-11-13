d3a58f1d88661e9679d7f22b450e2f964e37fefe
gut23014@byui.edu <matias1805ga@gmail.com>
2024-11-13 00:48:22 -0400

    changes for the current year

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,12 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const lastModifiedElement = document.getElementById("lastModified");
-    const currentYearElement = document.getElementById("year");
-
-
-    const currentYear = new Date().getFullYear();
-    currentYearElement.textContent = `© ${currentYear} Matias Gutierrez, Santa Cruz - Bolivia`;
-
-
-    const lastModifiedDate = new Date(document.lastModified);
-    lastModifiedElement.textContent = `Last modified: ${lastModifiedDate.toLocaleString()}`;
-});
+const yearSpan = document.querySelector('#currentyear');
+const currentYear = new Date();
+yearSpan.innerText = currentYear.getFullYear();
