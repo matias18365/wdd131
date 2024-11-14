ab95891ac191289b4c47566c718c16d65141a625
gut23014@byui.edu <matias1805ga@gmail.com>
2024-11-14 19:42:48 -0400

    Changes for the date and last modified

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,8 @@
-const yearSpan = document.querySelector('#currentyear');
-const currentYear = new Date();
-yearSpan.innerText = currentYear.getFullYear();
+document.addEventListener("DOMContentLoaded", function() {
+    const yearSpan = document.querySelector('#currentyear');
+    const currentYear = new Date();
+    yearSpan.innerText = currentYear.getFullYear();
+
+    const lastModifiedSpan = document.querySelector('#lastModified');
+    lastModifiedSpan.innerText = document.lastModified;
+});
