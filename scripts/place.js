8398e7bebab41fda9dcd49e67c9b00e4ecaf23b3
gut23014@byui.edu <matias1805ga@gmail.com>
2024-12-04 23:12:04 -0400

    I started the project using the temple project as a guide

---
+++ scripts/place.js
@@ -0,0 +1,18 @@
+//This is for the footer to show the date of the last update.
+document.addEventListener("DOMContentLoaded", function() {
+    const yearSpan = document.querySelector('#currentyear');
+    const currentYear = new Date();
+    yearSpan.innerText = currentYear.getFullYear();
+
+    const lastModifiedSpan = document.querySelector('#lastModified');
+    lastModifiedSpan.innerText = document.lastModified;
+});
+
+//This is for the menu button.
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
