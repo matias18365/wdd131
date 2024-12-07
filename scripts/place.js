7eff5ba5e2c5623c8a04bb9baf1f661ffc2f4c1e
gut23014@byui.edu <matias1805ga@gmail.com>
2024-12-07 00:09:30 -0400

    I added the js code

--- scripts/place.js
+++ scripts/place.js
@@ -1,4 +1,3 @@
-//This is for the footer to show the date of the last update.
 document.addEventListener("DOMContentLoaded", function() {
     const yearSpan = document.querySelector('#currentyear');
     const currentYear = new Date();
@@ -6,13 +5,26 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const lastModifiedSpan = document.querySelector('#lastModified');
     lastModifiedSpan.innerText = document.lastModified;
+
+
+    displayWindChill();
 });
 
-//This is for the menu button.
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
 
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
-});
+function displayWindChill() {
+    const temperature = 10;
+    const windSpeed = 5;
+    const windChillElement = document.querySelector('.weather p:last-of-type');
+
+
+    if (temperature <= 10 && windSpeed > 4.8) {
+        const windChill = calculateWindChill(temperature, windSpeed);
+        windChillElement.innerHTML = `<strong>Wind Chill:</strong> ${windChill.toFixed(1)} °C`;
+    } else {
+        windChillElement.innerHTML = `<strong>Wind Chill:</strong> N/A`;
+    }
+}
+
+function calculateWindChill(temp, speed) {
+    return 13.12 + (0.6215 * temp) - (35.75 * speed**0.16) + (0.4275 * temp * speed**0.16);
+}
