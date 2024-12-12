0e8139ec278efb9185313e7de199fb8ebcdb6c9f
gut23014@byui.edu <matias1805ga@gmail.com>
2024-12-12 00:13:44 -0400

    practicing w04

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,9 @@
+//This is for the footer to show the date of the last update.
+document.addEventListener("DOMContentLoaded", function() {
+    const yearSpan = document.querySelector('#currentyear');
+    const currentYear = new Date();
+    yearSpan.innerText = currentYear.getFullYear();
+
+    const lastModifiedSpan = document.querySelector('#lastModified');
+    lastModifiedSpan.innerText = document.lastModified;
+});
